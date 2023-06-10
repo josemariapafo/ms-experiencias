72e224c8e49d357dfab9d8f82cdbfdaeb94b404a
josemariapafo <parrillajosemaria@gmail.com>
2023-06-10 13:02:16 +0200

    Hacemos trabajar al micro con eureka

--- src/main/java/com/portafolio/msexperiencias/MsExperienciasApplication.java
+++ src/main/java/com/portafolio/msexperiencias/MsExperienciasApplication.java
@@ -2,10 +2,12 @@ package com.portafolio.msexperiencias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MsExperienciasApplication {
 
 	public static void main(String[] args) {
